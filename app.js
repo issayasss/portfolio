931172206af63636efbeb977f430f7bb47f59ace
Issa Adam Tidjani <126884755+issayasss@users.noreply.github.com>
2023-04-07 23:35:49 +0200

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const mediaButton = document.querySelector('#hamburger');
-
-const navItems = document.querySelector('.nav-items');
-
-mediaButton.addEventListener('click', function (e) {
-	mediaButton.classList.toggle('fa-burger');
-	mediaButton.classList.toggle('fa-xmark');
-	navItems.classList.toggle('show-list');
-});
